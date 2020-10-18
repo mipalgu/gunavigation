dc50c4e5dc0c7aea4950c06d8a0065254cb74d2b
Morgan McColl <morgan.mccoll@alumni.griffithuni.edu.au>
2020-10-18 15:17:30 +1000

    Made tests for including last reading ins tatus.

--- ctests/tracking_tests.cc
+++ ctests/tracking_tests.cc
@@ -23,6 +23,9 @@ namespace CGTEST {
         ASSERT_NEAR(rad_d_to_d(expected.turn), rad_d_to_d(actual.turn), 0.00001);
         ASSERT_EQ(cm_t_to_i(expectedCoord.x), cm_t_to_i(actualCoord.x));
         ASSERT_EQ(cm_t_to_i(expectedCoord.y), cm_t_to_i(actualCoord.y));
+        ASSERT_EQ(mm_t_to_i(expected.last_reading.forward), mm_t_to_i(actual.last_reading.forward));
+        ASSERT_EQ(mm_t_to_i(expected.last_reading.left), mm_t_to_i(actual.last_reading.left));
+        ASSERT_NEAR(rad_d_to_d(expected.last_reading.turn), rad_d_to_d(actual.last_reading.turn), 0.00001);
     }
 
     TEST_F(TrackingTests, CalculateDifference) {
@@ -53,7 +56,7 @@ namespace CGTEST {
         gu_cartesian_coordinate coord = {73, 150};
         gu_odometry_status currentStatus = {100, 20, deg_d_to_rad_d(d_to_deg_d(10.0)), coord, {}, lastReading};
         gu_cartesian_coordinate expectedCoord = {80, 100};
-        gu_odometry_status expected = {300, 400, deg_d_to_rad_d(d_to_deg_d(30.0)), expectedCoord, {}};
+        gu_odometry_status expected = {300, 400, deg_d_to_rad_d(d_to_deg_d(30.0)), expectedCoord, {}, currentReading};
         gu_odometry_status actual = track_coordinate(currentReading, currentStatus);
         compareStatus(expected, actual);
     }
@@ -64,7 +67,7 @@ namespace CGTEST {
         gu_cartesian_coordinate coord = {73, 150};
         gu_odometry_status currentStatus = {100, 200, deg_d_to_rad_d(d_to_deg_d(10.0)), coord, {}, lastReading};
         gu_cartesian_coordinate expectedCoord = {80, 100}; 
-        gu_odometry_status expected = {400, 600, deg_d_to_rad_d(d_to_deg_d(10.0)), expectedCoord, {}};
+        gu_odometry_status expected = {400, 600, deg_d_to_rad_d(d_to_deg_d(10.0)), expectedCoord, {}, currentReading};
         gu_odometry_status actual = track_coordinate(currentReading, currentStatus);
         compareStatus(expected, actual);
     }
@@ -77,7 +80,7 @@ namespace CGTEST {
         gu_odometry_status currentStatus = {100, 200, deg_d_to_rad_d(d_to_deg_d(10.0)), {}, relCoord, lastReading};
         gu_cartesian_coordinate expectedCoord = {80, 100}; 
         gu_relative_coordinate expectedRelative = cartesian_coord_to_rr_coord(expectedCoord);
-        gu_odometry_status expected = {400, 600, deg_d_to_rad_d(d_to_deg_d(10.0)), expectedCoord, expectedRelative};
+        gu_odometry_status expected = {400, 600, deg_d_to_rad_d(d_to_deg_d(10.0)), expectedCoord, expectedRelative, currentReading};
         gu_odometry_status actual = track_relative_coordinate(currentReading, currentStatus);
         compareStatus(expected, actual);
         gu_relative_coordinate actualRelative = actual.relative_coordinate;
@@ -91,7 +94,7 @@ namespace CGTEST {
         gu_cartesian_coordinate coord = {73, 150};
         gu_odometry_status currentStatus = {100, 20, deg_d_to_rad_d(d_to_deg_d(10.0)), coord, {}, lastReading};
         gu_cartesian_coordinate expectedCoord = {66, 200};
-        gu_odometry_status expected = {300, 400, deg_d_to_rad_d(d_to_deg_d(30.0)), expectedCoord, {}};
+        gu_odometry_status expected = {300, 400, deg_d_to_rad_d(d_to_deg_d(30.0)), expectedCoord, {}, currentReading};
         gu_odometry_status actual = track_self(currentReading, currentStatus);
         compareStatus(expected, actual);
     }
@@ -104,7 +107,7 @@ namespace CGTEST {
         gu_odometry_status currentStatus = {100, 200, deg_d_to_rad_d(d_to_deg_d(10.0)), {}, relCoord, lastReading};
         gu_cartesian_coordinate expectedCoord = {66, 200}; 
         gu_relative_coordinate expectedRelative = cartesian_coord_to_rr_coord(expectedCoord);
-        gu_odometry_status expected = {400, 600, deg_d_to_rad_d(d_to_deg_d(10.0)), expectedCoord, expectedRelative};
+        gu_odometry_status expected = {400, 600, deg_d_to_rad_d(d_to_deg_d(10.0)), expectedCoord, expectedRelative, currentReading};
         gu_odometry_status actual = track_self_relative(currentReading, currentStatus);
         compareStatus(expected, actual);
         gu_relative_coordinate actualRelative = actual.relative_coordinate;
